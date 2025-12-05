b63929ca44680afa27fd96f1b6f8f0e89b66032a
mnabil1718 <mnabil1718@gmail.com>
2025-12-05 11:03:10 +0700

    chore: refactor with web component

---
+++ scripts/components/project-card.js
@@ -0,0 +1,76 @@
+class ProjectCard extends HTMLElement {
+  set project(p) {
+    this._project = p;
+    this.render();
+  }
+
+  get project() {
+    return this._project;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  dateDelta(start, end) {
+    const s = new Date(start);
+    const e = new Date(end);
+
+    const ms = e - s;
+    const days = ms / (1000 * 60 * 60 * 24);
+    const months = Math.ceil(days / 30);
+
+    return `${months} bulan`;
+  }
+
+  render() {
+    if (!this._project) return;
+    const project = this._project;
+    const imageUrl = project.image ? URL.createObjectURL(project.image) : "#";
+    let techs = project.technology
+      .map((t) => `<li>${t}</li>`)
+      .join("");
+
+    this.innerHTML = `
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="project-card">
+            <img src="${imageUrl}" alt="${project.name}" />
+
+            <div class="content">
+              <h2>${project.name}</h2>
+
+              <span class="text-muted">
+                Durasi: ${this.dateDelta(project.startDate, project.endDate)}
+              </span>
+
+              <p class="mt-4">${project.description}</p>
+
+              <ul>
+                ${techs}
+              </ul>
+
+              <div class="buttons d-flex gap-2 mt-4">
+                <button class="btn btn-dark w-50 edit-btn">
+                  Edit
+                </button>
+
+                <button class="btn btn-dark w-50 delete-btn">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+    this.querySelector(".edit-btn").addEventListener("click", () => {
+      this.dispatchEvent(new CustomEvent("edit", { project: this._project }));
+    });
+
+    this.querySelector(".delete-btn").addEventListener("click", () => {
+      this.dispatchEvent(new CustomEvent("delete", { project: this._project }));
+    });
+  }
+}
+
+customElements.define("project-card", ProjectCard);
--- scripts/projects.js
+++ scripts/projects.js
@@ -5,45 +5,11 @@ const store = new StoreManager(STORE_NAME);
 const form = document.getElementById("project-form");
 const container = document.getElementById("project-list");
 
-function dateDelta(start, end) {
-  const s = new Date(start);
-  const e = new Date(end);
 
-  const ms = e - s;
-  const days = ms / (1000 * 60 * 60 * 24);
-  const months = Math.ceil(days / 30);
-
-  return `${months} bulan`;
-}
-
-function attachDeleteHandlers() {
-  document.querySelectorAll(".delete-btn").forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const id = btn.dataset.id;
-
-      store.delete({ id });
-      render();
-    });
-  });
-}
-
-function attachEditHandlers() {
-  document.querySelectorAll(".edit-btn").forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const id = btn.dataset.id;
-
-      const project = store.getById(id);
-
-      if (!project) {
-        return;
-      }
-
-      repopulateForm(project);
-    });
-  });
-}
 
 function render() {
+
+  container.innerHTML = "";
   const projects = store.getAll();
 
   if (projects.length === 0) {
@@ -51,50 +17,18 @@ function render() {
     return;
   }
 
-  container.innerHTML = projects
-    .map((p) => {
-      const techList = p.technology
-        .map((t) => `<li>${t}</li>`)
-        .join("");
+  projects.map((p) => {
+    const card = document.createElement("project-card");
+    card.project = p;
 
-      const imageUrl = p.image ? URL.createObjectURL(p.image) : "/assets/pas foto.jpg";
+    card.addEventListener("delete", (e) => {
+      const project = e.project;
+      store.delete({ id: project.id });
+      render();
+    });
 
-      return `
-        <div class="col-12 col-md-6 col-lg-4">
-          <div class="project-card">
-            <img src="${imageUrl}" alt="${p.name}" />
-
-            <div class="content">
-              <h2>${p.name}</h2>
-
-              <span class="text-muted">
-                Durasi: ${dateDelta(p.startDate, p.endDate)}
-              </span>
-
-              <p class="mt-4">${p.description}</p>
-
-              <ul>
-                ${techList}
-              </ul>
-
-              <div class="buttons d-flex gap-2 mt-4">
-                <button class="btn btn-dark w-50 edit-btn" data-id="${p.id}">
-                  Edit
-                </button>
-
-                <button class="btn btn-dark w-50 delete-btn" data-id="${p.id}">
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-    })
-    .join("");
-
-  attachDeleteHandlers();
-  attachEditHandlers();
+    container.appendChild(card);
+  });
 }
 
 function onSubmitHandler(e) {
@@ -134,8 +68,6 @@ function repopulateForm(project) {
     preview.src = URL.createObjectURL(project.image);
   }
 }
-
-
 
 render();
 
