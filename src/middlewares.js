c8308c29f7f90b840c14d89ace3139a268bbc52f
Muhammad Nabil <mnabil1718@gmail.com>
2025-12-17 07:45:15 +0700

    fix(upload): fix filename generator

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,21 @@
 import multer from "multer";
 import { config } from "./helper/config.js";
-const upload = multer({ dest: "src/public/uploads/" });
+import crypto from "node:crypto";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: "src/public/uploads",
+  filename: function (req, file, callback) {
+    const name_split = file.originalname.split(".");
+    const ext = path.extname(file.originalname); // .png, .jpg, etc
+    const base = path.basename(file.originalname, ext);
+    const id = crypto.randomBytes(6).toString("base64url");
+    const filename = `${id}-${base}${ext}`;
+
+    callback(null, filename);
+  },
+});
+const upload = multer({ storage });
 export const singleImageUploadMiddleware = upload.single("image");
 
 export const isAuthenticated = (req, res, next) => {
